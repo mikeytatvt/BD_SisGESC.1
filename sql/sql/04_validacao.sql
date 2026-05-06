efccb3323006afc627d5c536fbfb77dab3fd3f25
mikeytatvt <laredoni4@gmail.com>
2026-05-06 18:53:50 -0300

    Update and rename 04_validacao.sql to 04_validação_e_performance.sql
    Provar que os dados estão corretos usando SUM OLTP = SUM OLAP e assim criar índices

--- sql/sql/04_validacao.sql
+++
@@ -1,7 +0,0 @@
-SELECT COUNT(*) FROM aluno;
-SELECT COUNT(*) FROM fato_desempenho;
-
-SELECT SUM((nota_p1 + nota_p2)/2) FROM matricula;
-SELECT SUM(nota_total) FROM fato_desempenho;
-
-CREATE INDEX idx_fato_aluno ON fato_desempenho(fk_aluno);

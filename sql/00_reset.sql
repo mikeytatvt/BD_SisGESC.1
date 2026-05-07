5f12c9c758c71e427abbbbfea1aac365373d1a9f
mikeytatvt <laredoni4@gmail.com>
2026-05-07 17:21:13 -0300

    Delete sql/00_reset.sql

--- sql/00_reset.sql
+++
@@ -1,6 +0,0 @@
--- RESET E CRIAÇÃO DO BANCO
-DROP DATABASE IF EXISTS faculdade_nova_horizonte;
-DROP DATABASE IF EXISTS faculdade_NH_dw;
-
-CREATE DATABASE faculdade_nova_horizonte;
-CREATE DATABASE faculdade_NH_dw;

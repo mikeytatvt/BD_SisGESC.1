5f39e78e15402f67673e4d8f0b301e140e8db38f
mikeytatvt <laredoni4@gmail.com>
2026-05-07 17:19:19 -0300

    Update 01_estrutura_oltp.sql

--- sql/sql/01_estrutura_oltp.sql
+++ sql/sql/01_estrutura_oltp.sql
@@ -1,14 +1,37 @@
+-- RESET E CRIAÇÃO DO BANCO (Combinado para facilitar)
+DROP DATABASE IF EXISTS faculdade_nova_horizonte;
+DROP DATABASE IF EXISTS faculdade_NH_dw;
+
+CREATE DATABASE faculdade_nova_horizonte;
+CREATE DATABASE faculdade_NH_dw;
+
 -- TABELAS OLTP
 USE faculdade_nova_horizonte;
 
+-- ==========================================
 -- Módulo Acadêmico
+-- ==========================================
+CREATE TABLE tb_unidades(
+    pk_id_unidade INT AUTO_INCREMENT PRIMARY KEY,
+    nome_unidade VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE tb_cursos(
+    pk_id_curso INT AUTO_INCREMENT PRIMARY KEY,
+    nome_curso VARCHAR(100) NOT NULL,
+    fk_id_unidade INT NOT NULL,
+    FOREIGN KEY (fk_id_unidade) REFERENCES tb_unidades(pk_id_unidade)
+);
+
 CREATE TABLE tb_alunos(
     pk_rgm INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     nome_aluno VARCHAR(120) NOT NULL,
     data_nascimento DATE NOT NULL,
     cpf VARCHAR(11) UNIQUE,
     email VARCHAR(120) UNIQUE,
-    sexo CHAR(1)
+    sexo CHAR(1),
+    fk_id_curso INT NOT NULL,
+    FOREIGN KEY (fk_id_curso) REFERENCES tb_cursos(pk_id_curso)
 );
 
 CREATE TABLE tb_disciplinas(
@@ -19,7 +42,7 @@ CREATE TABLE tb_disciplinas(
 );
 
 CREATE TABLE tb_matriculas(
- pk_id_matricula INT AUTO_INCREMENT PRIMARY KEY,
+    pk_id_matricula INT AUTO_INCREMENT PRIMARY KEY,
     fk_rgm INT NOT NULL,
     fk_id_disciplina INT NOT NULL,
     data_matricula DATE NOT NULL,
@@ -27,7 +50,9 @@ CREATE TABLE tb_matriculas(
     FOREIGN KEY (fk_id_disciplina) REFERENCES tb_disciplinas(pk_id_disciplina)
 );
 
+-- ==========================================
 -- Módulo Financeiro
+-- ==========================================
 CREATE TABLE tb_contratos_educacionais(
     pk_id_contrato INT AUTO_INCREMENT PRIMARY KEY,
     fk_rgm INT NOT NULL,
@@ -45,7 +70,9 @@ CREATE TABLE tb_parcelas_contrato(
     FOREIGN KEY (fk_id_contrato) REFERENCES tb_contratos_educacionais(pk_id_contrato)
 );
 
+-- ==========================================
 -- Módulo RH
+-- ==========================================
 CREATE TABLE tb_funcionarios(
     pk_id_funcionario INT AUTO_INCREMENT PRIMARY KEY,
     nome_funcionario VARCHAR(120) NOT NULL,

deb87005b2ba1e607e350f25ca3298b11576ad4a
mikeytatvt <laredoni4@gmail.com>
2026-05-07 17:20:18 -0300

    Update and rename 03_dw_e_etl.sql to 03_operacoes_oltp.sql

---
+++ sql/sql/03_operacoes_oltp.sql
@@ -0,0 +1,22 @@
+USE faculdade_nova_horizonte;
+
+-- 1. Consulta Simples: Listar alunos e seus respectivos cursos
+SELECT a.nome_aluno, c.nome_curso 
+FROM tb_alunos a
+JOIN tb_cursos c ON a.fk_id_curso = c.pk_id_curso;
+
+-- 2. Subselect com Agregação: Alunos com contratos acima da média geral
+SELECT a.nome_aluno, ce.valor_total
+FROM tb_alunos a
+JOIN tb_contratos_educacionais ce ON a.pk_rgm = ce.fk_rgm
+WHERE ce.valor_total > (
+    SELECT AVG(valor_total) FROM tb_contratos_educacionais
+);
+
+-- 3. Subselect Correlacionado: Disciplinas que têm carga horária maior ou igual à média da faculdade
+SELECT d1.nome_disciplina, d1.carga_horaria
+FROM tb_disciplinas d1
+WHERE d1.carga_horaria >= (
+    SELECT AVG(carga_horaria) 
+    FROM tb_disciplinas d2
+);

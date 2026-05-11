dabd31849f2d5781a1063db974b578938a6da28f
mikeytatvt <laredoni4@gmail.com>
2026-05-11 16:23:59 -0300

    Update 05_validacao_e_performance.sql

--- sql/sql/05_validacao_e_performance.sql
+++ sql/sql/05_validacao_e_performance.sql
@@ -5,12 +5,12 @@ SELECT
     (SELECT SUM(valor_parcela) FROM faculdade_nova_horizonte.tb_parcelas_contrato WHERE data_pagamento IS NOT NULL) AS Soma_OLTP,
     (SELECT SUM(valor_pago) FROM fato_financeiro) AS Soma_OLAP;
 
--- 2. Performance: EXPLAIN ANTES da otimização
+-- 2. Performance: EXPLAIN ANTES da otimização (Mostra o custo alto inicial) [cite: 109]
 EXPLAIN SELECT * FROM fato_financeiro WHERE fk_tempo = 1;
 
 -- 3. Criação de Índices Estratégicos [cite: 26, 107]
 CREATE INDEX idx_fato_tempo ON fato_financeiro(fk_tempo);
 CREATE INDEX idx_fato_aluno ON fato_financeiro(fk_aluno);
 
--- 4. Performance: EXPLAIN DEPOIS da otimização (Demonstra o ganho de performance) [cite: 108, 109]
+-- 4. Performance: EXPLAIN DEPOIS da otimização (Prova o ganho de velocidade) [cite: 109]
 EXPLAIN SELECT * FROM fato_financeiro WHERE fk_tempo = 1;

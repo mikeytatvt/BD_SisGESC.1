edf37bca817e629efcb1aecd2644969e02b11525
mikeytatvt <laredoni4@gmail.com>
2026-05-11 17:35:43 -0300

    Update run_all.sql

--- sql/sql/run_all.sql
+++ sql/sql/run_all.sql
@@ -1,10 +1,10 @@
 -- Script unificado de instalação do projeto SisGESC
 -- Este arquivo executa todas as fases do projeto sequencialmente.
 
-SOURCE sql/01_estrutura_oltp.sql;
-SOURCE sql/02__carga_idempotente_dml.sql;
-SOURCE sql/03_operacoes_oltp.sql;
-SOURCE sql/04_dw_e_etl.sql;
-SOURCE sql/05_validacao_e_performance.sql;
+SOURCE sql/sql/01_estrutura_oltp.sql;
+SOURCE sql/sql/02__carga_idempotente_dml.sql;
+SOURCE sql/sql/03_operacoes_oltp.sql;
+SOURCE sql/sql/04_dw_e_etl.sql;
+SOURCE sql/sql/05_validacao_e_performance.sql;
 
 SELECT 'Instalação e Validação do SisGESC concluída com sucesso!' AS Status;

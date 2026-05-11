a8c5012bc50d413814726b918c6f6d7ffa9b101d
mikeytatvt <laredoni4@gmail.com>
2026-05-11 16:42:21 -0300

    Update run_all.sql

--- sql/sql/run_all.sql
+++ sql/sql/run_all.sql
@@ -2,7 +2,7 @@
 -- Este arquivo executa todas as fases do projeto sequencialmente.
 
 SOURCE sql/01_estrutura_oltp.sql;
-SOURCE sql/02_carga_idempotente_dml.sql;
+SOURCE sql/02__carga_idempotente_dml.sql;
 SOURCE sql/03_operacoes_oltp.sql;
 SOURCE sql/04_dw_e_etl.sql;
 SOURCE sql/05_validacao_e_performance.sql;

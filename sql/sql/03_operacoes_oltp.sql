b666f077ab40f631fac84d96a21a1290caff6beb
mikeytatvt <laredoni4@gmail.com>
2026-05-11 17:43:35 -0300

    Update 03_operacoes_oltp.sql

--- sql/sql/03_operacoes_oltp.sql
+++ sql/sql/03_operacoes_oltp.sql
@@ -1,22 +1,44 @@
 USE faculdade_nova_horizonte;
 
--- 1. Consulta Simples: Listar alunos e seus respectivos cursos
-SELECT a.nome_aluno, c.nome_curso 
-FROM tb_alunos a
-JOIN tb_cursos c ON a.fk_id_curso = c.pk_id_curso;
+-- ==========================================
+-- CONSULTA 1
+-- LISTAR ALUNOS E CURSOS
+-- ==========================================
 
--- 2. Subselect com Agregação: Alunos com contratos acima da média geral
-SELECT a.nome_aluno, ce.valor_total
+SELECT
+    a.nome_aluno,
+    c.nome_curso
 FROM tb_alunos a
-JOIN tb_contratos_educacionais ce ON a.pk_rgm = ce.fk_rgm
+INNER JOIN tb_cursos c
+    ON a.fk_id_curso = c.pk_id_curso;
+
+-- ==========================================
+-- CONSULTA 2
+-- CONTRATOS ACIMA DA MÉDIA
+-- ==========================================
+
+SELECT
+    a.nome_aluno,
+    ce.valor_total
+FROM tb_alunos a
+INNER JOIN tb_contratos_educacionais ce
+    ON a.pk_rgm = ce.fk_rgm
 WHERE ce.valor_total > (
-    SELECT AVG(valor_total) FROM tb_contratos_educacionais
+    SELECT AVG(valor_total)
+    FROM tb_contratos_educacionais
 );
 
--- 3. Subselect Correlacionado: Disciplinas que têm carga horária maior ou igual à média da faculdade
-SELECT d1.nome_disciplina, d1.carga_horaria
+-- ==========================================
+-- CONSULTA 3
+-- DISCIPLINAS COM CARGA HORÁRIA
+-- ACIMA OU IGUAL À MÉDIA
+-- ==========================================
+
+SELECT
+    d1.nome_disciplina,
+    d1.carga_horaria
 FROM tb_disciplinas d1
 WHERE d1.carga_horaria >= (
-    SELECT AVG(carga_horaria) 
+    SELECT AVG(d2.carga_horaria)
     FROM tb_disciplinas d2
 );
